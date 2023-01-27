b233ce3ec462ec4e242bd6bd8d38e7f651aca310
karenis32 <karen.is32@hotmail.com>
2023-01-27 16:33:03 -0300

    Agrego migraciones

---
+++ SLNClinica/SLNClinica/Migrations/20230127193209_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SLNClinica.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Apellido = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    Matricula = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
