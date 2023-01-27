801e0dfacc628c9036c520834c6281e4a87a4041
karenis32 <karen.is32@hotmail.com>
2023-01-27 16:07:26 -0300

    Subo models

---
+++ SLNClinica/SLNClinica/Models/Medico.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SLNClinica.Models
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]//view/ef
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]//view
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Campo obligatorio")]//view/ef
+        [Column(TypeName = "varchar(50)")]
+        [StringLength(50)]//view
+        public string Apellido { get; set; }
+
+        [RegularExpression("@^[A-Z]{2}[0-9]{4}")]
+        public int Matricula { get; set; }
+
+    }
+}
